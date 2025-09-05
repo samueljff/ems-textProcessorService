649be3023ca950457decf53a1f4f45edef1d8887
samueljff <samueljosef3@gmail.com>
2025-09-04 23:02:04 -0300

    Inital project

---
+++ src/main/java/com/fonseca/algaposts/textProcessorService/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fonseca.algaposts.textProcessorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fonseca/algaposts/textProcessorService/TextProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonseca.algaposts.textProcessorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
